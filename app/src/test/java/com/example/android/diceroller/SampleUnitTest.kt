f0876613c5aa8b1aa2e904c82ff8c214e6d87a15
nithin <nithin.p@experionglobal.com>
2019-11-20 18:57:54 +0530

    - unit test class added

---
+++ app/src/test/java/com/example/android/diceroller/SampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.example.android.diceroller
+
+import org.junit.Assert
+import org.junit.Test
+
+class SampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+}
